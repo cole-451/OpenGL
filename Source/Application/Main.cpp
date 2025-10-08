258e4665cb99e0f6c3502d8c61a45a58e046e454
cole-451 <coleneely87@gmail.com>
2025-10-08 10:16:23 -0600

    Can't see anything, I hate this.

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -8,6 +8,27 @@ int main(int argc, char* argv[]) {
     neu::GetEngine().Initialize();
 
     // initialize scene
+    std::vector<neu::vec3> points;
+    std::vector<neu::vec3> colors;
+
+    neu::vec3 color1{ 1,0,1 };
+    neu::vec3 color2{ 1,0,1 };
+    neu::vec3 color3{ 1,0,1 };
+    neu::vec3 color4{ 1,0,1 };
+    colors.push_back(color1);
+    colors.push_back(color2);
+    colors.push_back(color3);
+    colors.push_back(color4);
+
+    neu::vec3 point1{ -1,1,0 };
+    points.push_back(point1);
+    neu::vec3 point2{ 1,0,0 };
+    points.push_back(point2);
+    neu::vec3 point3{ 1,1,0 };
+    neu::vec3 point4{ 1, -1, 0 };
+    points.push_back(point3);
+    points.push_back(point4);
+
 
 
     SDL_Event e;
@@ -26,21 +47,51 @@ int main(int argc, char* argv[]) {
 
         if (neu::GetEngine().GetInput().GetKeyPressed(SDL_SCANCODE_ESCAPE)) quit = true;
 
+        //USE THIS FOR TRANSFORMATION.
+        neu::vec2 position = neu::GetEngine().GetInput().GetMousePosition();
+
+        float angle = neu::GetEngine().GetTime().GetTime() * 90.0f;
+
+
+
+        std::cout << neu::GetEngine().GetInput().GetMousePosition().x<<std::endl;
+        std::cout << neu::GetEngine().GetInput().GetMousePosition().y<<std::endl;
+
         // draw
         neu::GetEngine().GetRenderer().Clear();
 
+        glLoadIdentity();
+        glPushMatrix();
+        glTranslatef(position.x, position.y, 0);
+       // glRotatef(angle, 1, 0, 0); make an angle variable
+        //glScalef(scale, scale, 0);
         glBegin(GL_QUADS);
 
-        glColor3f(1,0,0);
+       /* for (neu::vec3& color: colors) {
+            glColor3f(color.r, color.g, color.b);
+        }
+        for (neu::vec3& point : points) {
+            glColor3f(point.x, point.y, point.z);
+        }*/
+
+        for (int i = 0; i < points.size(); i++) {
+            glColor3f(colors[i].r, colors[i].g, colors[i].b);
+            glVertex3f(points[i].x, points[i].y, points[i].z);
+        }
+
+
+        /*glColor3f(1,0,0);
         glVertex3f(-1,1,0);
         glColor3f(1,1,0);
         glVertex3f(1,0,0);
         glColor3f(1,1,0);
         glVertex3f(1,1,0);
-        glColor4f(1, 0, 1, 0);
-        glVertex4f(1, -1, 0, 10);
+        glColor3f(1, 0, 1);
+        glVertex3f(1, -1, 0);*/
 
         glEnd();
+
+        glPopMatrix();
 
         neu::GetEngine().GetRenderer().Present();
 
