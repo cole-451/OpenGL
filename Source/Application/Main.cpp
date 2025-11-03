e98fd8ef6d46ac41b010a28b48531276e3d0b503
cole-451 <coleneely87@gmail.com>
2025-11-03 09:37:25 -0700

    populated scene

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -7,45 +7,17 @@ int main(int argc, char* argv[]) {
 	LOG_INFO("initialize engine...");
 	neu::GetEngine().Initialize();
 
-	// initialize OpenGL stuff
-	std::vector<neu::vec3> points{ { -0.5f, -0.5f, 0 }, { 0, 0.5f, 0 }, { 0.5f, -0.5f, 0 }, { 0.5f, 0.5f, 0 } };
-	std::vector<neu::vec3> colors{ { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 }, {1,0,1} };
-	std::vector<neu::vec2> texturecoords{ {0,0}, {0.5f, 1.0f}, {1,1} };
+	SDL_Event e;
+	bool quit = false;
 
+	
+
+	//load scene
 	auto scene = std::make_unique<neu::Scene>();
 	scene->Load("scenes/scene01.json");
 
 
-	struct Vertex {
-		neu::vec3 position;
-		neu::vec3 color;
-		neu::vec2 texturecoords;
-	};
-
-	std::vector<Vertex> vertices{
-		{ { -0.5f, -0.5f, 0 }, { 1, 0, 0 },{0,0} },
-		{{ -0.5f, 0.5f, 0 }, { 0, 1, 0 },{0.0f, 1.0f} },
-		{{ 0.5f, 0.5f, 0 },{ 0, 0, 1 },{1,1} },
-		{{ 0.5f, -0.5f, 0 },{ 0, 0, 1 },{1,0} }
-	};
-
-	std::vector<GLshort> indices{ 0, 1, 2, 2, 3, 0 };
-
-	//vertex buffer
-	neu::res_t<neu::VertexBuffer> vb = std::make_shared<neu::VertexBuffer>();
-	vb->CreateVertexBuffer((GLsizei)sizeof(Vertex)* vertices.size(), (GLsizei)vertices.size(), vertices.data());
-	vb->CreateIndexBuffer(GL_UNSIGNED_SHORT, (GLsizei)indices.size(), indices.data());
-
-	vb->SetAttribute(0, 3, sizeof(Vertex), offsetof(Vertex, position));
-	vb->SetAttribute(1, 3, sizeof(Vertex), offsetof(Vertex, color));
-	vb->SetAttribute(2, 2, sizeof(Vertex), offsetof(Vertex, texturecoords));
 	
-
-
-
-
-	SDL_Event e;
-	bool quit = false;
 
 	// MAIN LOOP
 	while (!quit) {
@@ -64,7 +36,9 @@ int main(int argc, char* argv[]) {
 
 		scene->Update(dt);
 		
-		/*float speed = 10.0f;
+
+		float speed = 10.0f;
+		/*
 		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_A)) camera.position.x -= speed * dt;
 		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_D)) camera.position.x += speed * dt;
 
@@ -72,25 +46,13 @@ int main(int argc, char* argv[]) {
 		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_S)) camera.position.y -= speed * dt;
 
 		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_Q)) camera.position.z += speed * dt;
-		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_E)) camera.position.z -= speed * dt;*/
+		if (neu::GetEngine().GetInput().GetKeyDown(SDL_SCANCODE_E)) camera.position.z -= speed * dt;
 
+		*/
 
 		
 
 
-
-
-		//USE THIS FOR TRANSFORMATION.
-		neu::vec2 mousepos = neu::GetEngine().GetInput().GetMousePosition();
-		neu::vec2 position;
-		position.x = neu::math::Remap(0.0f, (float)neu::GetEngine().GetRenderer().GetWidth(), -1.0f, 1.0f, mousepos.x);
-		position.y = -neu::math::Remap(0.0f, (float)neu::GetEngine().GetRenderer().GetHeight(), -1.0f, 1.0f, mousepos.y);
-
-
-		float angle = neu::GetEngine().GetTime().GetTime() * 90.0f;
-		neu::vec2 scale;
-		scale.x = neu::math::sin(neu::GetEngine().GetTime().GetTime());
-		scale.y = neu::math::sin(neu::GetEngine().GetTime().GetTime());
 
 		// draw
 		neu::GetEngine().GetRenderer().Clear();
@@ -100,6 +62,9 @@ int main(int argc, char* argv[]) {
 		ImGui::NewFrame();
 		// set ImGui
 		
+		ImGui::Begin("Editor");
+		
+
 		
 		ImGui::End();
 
